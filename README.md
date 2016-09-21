3b228c8f6facc59c857ab42469f11d2023a809d3
cyberplanner <saleh.gfx@gmail.com>
2016-09-21 21:23:45 +0100

    Updates.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # bank_kata_ruby
 
+![alt text](http://freefall4film.com/work-dir/screenshots/bank_kata/s.png "Screenshot")
 #### to run the program:
  - clone it!
  - run `bundle`
--- lib/app.rb
+++ lib/app.rb
@@ -15,12 +15,27 @@ end
 
 def print_menu
   puts ""
-  puts "Welcome to Your Bank"
+  puts "Welcome to Your Bank".center(70)
+  puts "-------------".center(70)
   puts "1. " + Rainbow("View balance").underline.bright
   puts "2. " + Rainbow("View transactions").underline.bright
   puts "3. " + Rainbow("Deposit").underline.bright
   puts "4. " + Rainbow("Withdraw").underline.bright
   puts "9. " + Rainbow("Exit").red.underline.bright
+end
+
+def deposit
+  puts "How much would you like to deposit: "
+  input = gets.chomp.to_i
+  @account.deposit(input)
+  puts "Thanks! £#{input} were added to your balance. current balance: £#{@account.balance}"
+end
+
+def withdraw
+  puts "How much would you like to withdraw: "
+  input = gets.chomp.to_i
+  @account.withdraw(input)
+  puts "Thanks! £#{input} were deducted from your balance. current balance: £#{@account.balance}"
 end
 
 def interactive_menu
@@ -31,26 +46,28 @@ def interactive_menu
   end
 end
 
+def print_transactions
+  puts "date || credit || debit || balance"
+  @account.transactions.each do |trans|
+    puts "#{trans.date} || #{trans.amount} || #{trans.current_debit} || #{trans.current_balance}"
+  end
+end
+
+def view_balance
+  puts "Your account balance is £#{@account.balance}"
+end
+
+
 def process(selection)
   case selection
   when "1"
-    puts "Your account balance is £#{@account.balance}"
-  when "2"
-    puts "date || credit || debit || balance"
-    @account.transactions.each do |trans|
-      puts "#{trans.date} || #{trans.amount} || #{trans.current_debit} || #{trans.current_balance}"
-    end
-  when "3"
-    puts "How much would you like to deposit: "
-    input = gets.chomp.to_i
-    @account.deposit(input)
-    puts "Thanks! £#{input} were added to your balance. current balance: £#{@account.balance}"
-  when "4"
-    puts "How much would you like to withdraw: "
-    input = gets.chomp.to_i
-    @account.withdraw(input)
-    puts "Thanks! £#{input} were deducted from your balance. current balance: £#{@account.balance}"
 
+  when "2"
+    print_transactions
+  when "3"
+    deposit
+  when "4"
+    withdraw
   when "9"
     termination
   end
